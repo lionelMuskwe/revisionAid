3f73deb630a395d563c6a3e6010a51f229902285
lionelMuskwe <lionel04muskwe@gmail.com>
2022-11-16 23:27:30 +0000

    Added toString functions for all the models

--- Question/models.py
+++ Question/models.py
@@ -6,13 +6,25 @@ class Subject(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
 class Topic(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Question(models.Model):
     question = models.CharField(max_length=100)
     answer = models.CharField(max_length=100)
     explanation = models.CharField(max_length=100, null=True, blank=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.question
