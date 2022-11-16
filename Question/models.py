01621e939e070bfd71dcf94c048c2beae5c9b8d5
lionelMuskwe <lionel04muskwe@gmail.com>
2022-11-16 05:42:18 +0000

    Set up question model

---
+++ Question/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    explanation = models.CharField(max_length=100, null=True, blank=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
