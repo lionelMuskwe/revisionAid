46474b399f67c9a501ccd1930114a9029992d1cc
lionelMuskwe <lionel04muskwe@gmail.com>
2022-11-16 23:40:32 +0000

    Set allowed hosts to any

--- revisionAid/settings.py
+++ revisionAid/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#4tyr$b^0hjxo*qv5r2+c%8lvyg%bv@wg3)sc9uuo0i*ns1h81
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
