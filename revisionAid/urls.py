78fc432027368e9dc538ddba137e626d05a9ce45
lionelMuskwe <lionel04muskwe@gmail.com>
2022-11-17 00:58:52 +0000

    Added static url and imported a few dependencies in the revisionAid.urls

--- revisionAid/urls.py
+++ revisionAid/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Interface.urls")),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
