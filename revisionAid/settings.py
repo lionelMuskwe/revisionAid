b83bab16820216a2ca5867a52f68e45a636af038
lionelMuskwe <lionel04muskwe@gmail.com>
2022-11-17 01:48:20 +0000

    Added extra DIRS array
    Trying to solve production template issues

--- revisionAid/settings.py
+++ revisionAid/settings.py
@@ -58,9 +58,8 @@ ROOT_URLCONF = 'revisionAid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
-        'DIRS': [BASE_DIR + "/templates", ],
-
+        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
