2848987d4a2c786241d496853681988e4dd5c5a6
lionelMuskwe <lionel04muskwe@gmail.com>
2022-11-17 01:38:57 +0000

    Added extra DIRS array
    Trying to solve production template issues

--- revisionAid/settings.py
+++ revisionAid/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'revisionAid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': ["templates"],
+        'DIRS': [BASE_DIR + "/templates", ],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
